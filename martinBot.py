7c88b682df720b74dcf5f22d2719f9f2a7dff8eb
Martin <martinwang7963@gmail.com>
2019-09-27 18:08:30 +0800

    Add Webhook setting

--- martinBot.py
+++ martinBot.py
@@ -286,9 +286,11 @@ def error(update, context):
 
 
 def main():
-
+    token = config["Default"]["BotApiKey"]
+    port = int(os.environ.get('PORT', '8443'))
+    webhook = config["Default"]["WebhookUrl"]
     updater = Updater(
-        config["Default"]["BotApiKey"], use_context=True)
+        token, use_context=True)
 
     dp = updater.dispatcher
 
@@ -328,7 +330,13 @@ def main():
     dp.add_handler(conv_handler)
     dp.add_error_handler(error)
 
-    updater.start_polling()
+    # updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=port,
+                          url_path=token)
+
+
+    updater.bot.set_webhook(webhook + token)
     updater.idle()
 
 
